846bd4bac96e40c314757d8df04132c3d5f62244
hyeyeon-lee <dear.yeonee@gmail.com>
2021-01-11 06:15:14 +0900

    TestController 추가
    /home

---
+++ study/src/main/java/com/hyeyeon/study/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.hyeyeon.study.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+	@RequestMapping(value = "/home")
+	public String home() {
+		return "index.html";
+	}
+
+}
